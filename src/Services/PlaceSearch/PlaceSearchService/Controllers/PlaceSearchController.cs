08e5cd1f014ab37d6a7fe7ee314d603fee0daafa
Piotr Pacyna <piotr@pacyna.dev>
2020-04-25 18:44:44 +0200

    Initial commit for Places solution with PlaceSearch service

---
+++ src/Services/PlaceSearch/PlaceSearchService/Controllers/PlaceSearchController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PlaceSearchService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlaceSearchController : ControllerBase
+    {
+        private readonly ILogger<PlaceSearchController> logger;
+
+        public PlaceSearchController(ILogger<PlaceSearchController> logger)
+        {
+            this.logger = logger;
+        }
+    }
+}
